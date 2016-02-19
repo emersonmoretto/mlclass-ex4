91eac04b0e293f257b602c2c21e7c365c7bd4971
baixiaoyue <baixiaoyue2014@hotmail.com>
2016-02-19 17:57:45 +0200

    line14
    I think actually the h2 is the result instead of a hide layer. Therefore no need to compute the sigmoid value of h2. Just from myself's opinion.

--- predict.m
+++ predict.m
@@ -11,7 +11,7 @@ num_labels = size(Theta2, 1);
 p = zeros(size(X, 1), 1);
 
 h1 = sigmoid([ones(m, 1) X] * Theta1');
-h2 = sigmoid([ones(m, 1) h1] * Theta2');
+h2 = [ones(m, 1) h1] * Theta2';
 [dummy, p] = max(h2, [], 2);
 
 % =========================================================================
